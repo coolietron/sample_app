740d7e44e3e3c49df2d7d3dfeb9c9c519ae868ea
Adam Ng <adamjng@gmail.com>
2012-08-05 18:30:08 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,31 @@ class UsersController < ApplicationController
   before_filter :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to root_path
+      flash[:failure] = "Access to 'new' action denied."
+    else
+      @user = User.new
+    end
   end
   
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    
+    if signed_in?
+      redirect_to root_path
     else
-      render 'new'
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
   
@@ -48,12 +59,6 @@ class UsersController < ApplicationController
   
 private 
     
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_path, notice: "Please sign in."
-      end
-    end
   
     def correct_user
       @user = User.find(params[:id])
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,10 +32,19 @@ describe "UserPages" do
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1)  { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2)  { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
-    
-    it { should have_selector('h1', text: user.name) }
+
+    it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
   
   describe "signup_page" do
@@ -66,10 +75,10 @@ describe "UserPages" do
     
     describe "with valid information" do
       before do
-        fill_in "Name",             with: "Example User"
-        fill_in "Email",            with: "user@example.com"
-        fill_in "Password",         with: "foobar"
-        fill_in "Confirmation",     with: "foobar"
+        fill_in "Name",                 with: "Example User"
+        fill_in "Email",                with: "user@example.com"
+        fill_in "Password",             with: "foobar"
+        fill_in "Confirm Password" ,    with: "foobar"
       end
       
       it "should create a user" do
